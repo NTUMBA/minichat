168b92f605de4806a066333623b015639f1a131c
NTUMBA <cityzenmboma@yahoo.fr>
2016-11-07 12:40:24 +0100

    Delete a text into mimnichat_post

--- minichat_post.php
+++ minichat_post.php
@@ -2,7 +2,7 @@
 // Connexion à la base de données
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'dvlpnum');
+	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
 }
 catch(Exception $e)
 {
